2240ba6ad488218ccadd4c7bf6ef0faa2487eb79
Adrian Sieber <mail@adriansieber.com>
2025-10-26 15:31:16 +0000

    Show commands before options in CLI help output

--- bin/cli.ts
+++ bin/cli.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env bun
 
-import { program } from "commander"
+import { program, Help, Command, Option } from "commander"
 import fs from "fs"
 import packageFile from "../package.json" with { type: "json" }
 import mardow from "../index.js"
@@ -42,10 +42,55 @@ program
     }
   })
 
+// Custom help class to show commands before options
+class CustomHelp extends Help {
+  override formatHelp (cmd: Command, helper: Help): string {
+    const termWidth = helper.padWidth(cmd, helper)
+    const output = [`Usage: ${helper.commandUsage(cmd)}`, ""]
+
+    if (cmd.description()) {
+      output.push(cmd.description(), "")
+    }
+
+    // Commands section BEFORE options
+    const commandList = helper.visibleCommands(cmd)
+      .map((subCmd: Command) => {
+        return helper.formatItem(
+          helper.subcommandTerm(subCmd),
+          termWidth,
+          helper.subcommandDescription(subCmd),
+          helper,
+        )
+      })
+    if (commandList.length > 0) {
+      output.push(...helper.formatItemList("Commands:", commandList, helper))
+      output.push("")
+    }
+
+    // Options section AFTER commands
+    const optionList = helper.visibleOptions(cmd)
+      .map((option: Option) => {
+        return helper.formatItem(
+          helper.optionTerm(option),
+          termWidth,
+          helper.optionDescription(option),
+          helper,
+        )
+      })
+    if (optionList.length > 0) {
+      output.push(...helper.formatItemList("Options:", optionList, helper))
+      output.push("")
+    }
+
+    return output.join("\n")
+  }
+}
+
 program
   .version(packageFile.version)
   .usage("[command] [options] [file]")
   .description("")
+  .createHelp = () => new CustomHelp()
 
 
 program.parse(process.argv)
